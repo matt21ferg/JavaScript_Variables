d593099026e7934d4729bd1355f8ed74baa3d263
Matthew Ferguson <you@example.com>
2022-02-11 11:19:17 -0600

    over with

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log(
 );
 
 // Exercise 1
+var age = 20;
+const firstName = "Matthew"
+let lastName = "Ferguson"
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`
+console.log(fullName)
 
 // Exercise 3
+let city = "Hartselle Alabama"
+let pastTime  = "and I love playing basketball "
+let myStory = `Hi, my name is ${fullName} and I am from ${city} ${pastTime} and coding!`
+console.log(myStory)
